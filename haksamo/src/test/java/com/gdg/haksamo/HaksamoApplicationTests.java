876b48dc57011244523fea0f78c308f7cf655f96
김동우 <dongwoo143@gmail.com>
2026-05-19 21:07:05 +0900

    chore: 초기 springboot 프로젝트 생성

---
+++ haksamo/src/test/java/com/gdg/haksamo/HaksamoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdg.haksamo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaksamoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
