62c677aa90a29de92694cd0e142f9df47cbd7918
llkloty <gimchaeyun188@gmail.com>
2026-05-25 16:29:22 +0900

    feat: 학식 메뉴 크롤링 및 DB 저장 기능 구현
    - Jsoup으로 경북대 생협 사이트 6개 식당 메뉴 파싱
    - Menu 엔티티, MenuRepository, MenuCrawlerService 추가
    - 식당별 HTML 구조 차이 처리 (ownText + p태그 조합)
    - 메뉴 이름 정제 (시간/특식 표시/라벨 regex 제거)
    - 특식 여부: 정보센터·첨성·공식당학생의 끼니별 첫 메뉴
    - @Scheduled(cron = "0 0 1 * * MON") 매주 월요일 자동 실행
    
    - closes #3

---
+++ haksamo/src/main/java/com/gdg/haksamo/domain/menu/Menu.java
@@ -0,0 +1,28 @@
+package com.gdg.haksamo.domain.menu;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Menu {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String restaurant;
+    private String week;
+    private String time;
+    private int price;
+    private boolean special;
+}
---
+++ haksamo/src/main/java/com/gdg/haksamo/domain/menu/MenuCrawlerService.java
@@ -0,0 +1,102 @@
+package com.gdg.haksamo.domain.menu;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+public class MenuCrawlerService {
+    private final MenuRepository menuRepository;
+
+    public MenuCrawlerService(MenuRepository menuRepository) {
+        this.menuRepository = menuRepository;
+    }
+
+    @Scheduled(cron = "0 0 1 * * MON")
+    void crawl() {
+        try {
+            restaurant("https://coop.knu.ac.kr/sub03/sub01_01.html?shop_sqno=35", "정보센터");
+            restaurant("https://coop.knu.ac.kr/sub03/sub01_01.html?shop_sqno=36", "복지관");
+            restaurant("https://coop.knu.ac.kr/sub03/sub01_01.html?shop_sqno=37", "첨성");
+            restaurant("https://coop.knu.ac.kr/sub03/sub01_01.html?shop_sqno=46", "글플");
+            restaurant("https://coop.knu.ac.kr/sub03/sub01_01.html?shop_sqno=85", "공식당 교직원");
+            restaurant("https://coop.knu.ac.kr/sub03/sub01_01.html?shop_sqno=86", "공식당 학생");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void restaurant(String url, String restaurantName) throws IOException {
+        Document doc = Jsoup.connect(url).get();
+        Elements div = doc.select("div.week_table");
+
+        for (Element di : div) {
+            if (di.select("p.title").text().equals("조식")) {
+                time(di.select("td"), restaurantName, "조식");
+            }
+            if (di.select("p.title").text().equals("중식")) {
+                time(di.select("td"), restaurantName, "중식");
+            }
+            if (di.select("p.title").text().equals("석식")) {
+                time(di.select("td"), restaurantName, "석식");
+            }
+        }
+    }
+
+    void time(Elements tdList, String restaurantName, String mealTime) {
+        Element[] week = new Element[5];
+        String[] days = {"월요일", "화요일", "수요일", "목요일", "금요일"};
+
+        for (int i = 0; i < 5; i++) {
+            week[i] = tdList.get(i);
+
+            boolean isFirst = true;
+            boolean isSpecialRestaurant = restaurantName.equals("정보센터")
+                    || restaurantName.equals("첨성")
+                    || restaurantName.equals("공식당 학생");
+
+            for (Element menu : week[i].select("li")) {
+                Menu menuEntity = new Menu();
+                if (menu.select("p").size() == 2) {
+                    String name = (menu.ownText() + " " + menu.select("p").get(0).text()).trim();
+                    name = name.replaceAll("\\s*★\\s*", "");
+                    name = name.replaceAll("\\s*운영시간.*", "");
+                    name = name.replaceAll("\\s*정식\\s*", "");
+                    name = name.replaceAll("\\s*천원의 아침밥\\s*", "");
+                    name = name.replaceAll("\\s*\\(2인\\)\\s*", "");
+                    name = name.replaceAll("\\s*\\(\\d:\\d{2}~\\d{2}:\\d{2}, \\d{2}:\\d{2}~\\d{2}:\\d{2}\\)\\s*", "");
+                    name = name.replaceAll("\\s*\\(\\d{2}:\\d{2}~\\d{2}:\\d{2}\\)\\s*", "");
+                    name = name.trim();
+                    menuEntity.setName(name);
+
+                    menuEntity.setRestaurant(restaurantName);
+                    menuEntity.setTime(mealTime);
+                    menuEntity.setWeek(days[i]);
+
+                    menuEntity.setSpecial(isFirst && isSpecialRestaurant);
+                    isFirst = false;
+
+                    String priceStr = menu.select("p").get(1).text();
+                    int price = Integer.parseInt(priceStr.replace("￦ ", "").replace(",", ""));
+                    menuEntity.setPrice(price);
+                    menuRepository.save(menuEntity);
+                } else if (menu.text().contains("0원의 저녁밥")) {
+                    String name = (menu.ownText() + " " + menu.select("p").get(0).text()).trim();
+                    name = name.replaceAll("\\s*\\*0원의 저녁밥\\*\\s*", "");
+                    menuEntity.setName(name);
+
+                    menuEntity.setRestaurant(restaurantName);
+                    menuEntity.setTime(mealTime);
+                    menuEntity.setWeek(days[i]);
+                    menuEntity.setPrice(0);
+                    menuRepository.save(menuEntity);
+                }
+            }
+        }
+    }
+}
