8a8ed06afda9795de71c5d26f0f85d0b7165634b
wnhaoo <153691178+wnhaoo@users.noreply.github.com>
2026-06-23 19:05:27 +0900

    chore: 임시 Security 설정 추가

---
+++ haksamo/src/main/java/com/gdg/haksamo/global/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.gdg.haksamo.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * 임시 Security 설정
+ * JWT 인증 구현 후 이 파일 삭제 or 교체
+ */
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                );
+        return http.build();
+    }
+}
